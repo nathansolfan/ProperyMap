ef1918560ee17941f715fcf77fcd802c8ddc7172
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-08-15 12:44:12 +0100

    Upddates

--- app/Models/Postcode.php
+++ app/Models/Postcode.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Postcode extends Model
 {
-    //
+    protected $fillable =  [
+        'postcode',
+        'district',
+        'avg_price',
+        'property_count',
+        'latitude',
+        'longitude',
+        'created_at',
+        'updated_at'
+        ];
+
+    public function hasMany()
+    {
+        return $this->hasMany(Property::class);
+    }
 }

3480cf1cbc75357b4e8ffd618a29e708a51b5aff
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-08-16 17:10:51 +0100

    Upddates

--- app/Services/PropertyService.php
+++ app/Services/PropertyService.php
@@ -5,4 +5,31 @@ namespace App\Services;
 class PropertyService
 {
 
+    public function liverpoolData()
+    {
+
+    }
+
+    public function getPriceByYear()
+    {
+        $areas = [
+            'L1' => [
+                'name' => 'City Centre',
+                'lat' => 53.4084,
+                'lng' => -2.9916,
+                'base_price' => 280000
+            ],
+            'L2' => [
+                'name' => 'Business District',
+                'lat' => 53.4094,
+                'lng' => -2.9856,
+                'base_price' => 250000
+            ]
+        ];
+
+        return $areas;
+
+    }
+
+
 }

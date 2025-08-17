dd3018f06474776b95bba83f023f90ee5e43958a
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-08-17 23:58:00 +0100

    Upddates

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\PropertyService;
 use Illuminate\Http\Request;
 
 class PropertyController extends Controller
 {
-    //
+
+    protected $propertyService;
+    public function __construct(PropertyService $propertyService)
+    {
+        $this->propertyService = $propertyService;
+    }
+
+    public function index()
+    {
+        $property = $this->propertyService->getPriceByYear();
+
+        return view('/')
+
+    }
 }

2166378ed1d65e9c353cf4e1299548ba136aa1c6
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-08-25 20:02:49 +0100

    Upddates

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,9 @@ Route::get('/', function () {
 });
 
 //Route::get('/properties', [PropertyController::class, 'index']);
-Route::get('/properties/{search}', [PropertyController::class, 'search']);
-Route::post('/properties/search', [PropertyController::class, 'searchPost']);
-Route::post('/properties/search-street', [PropertyController::class, 'searchByStreet']);
+//Route::get('/properties/{search}', [PropertyController::class, 'search']);
+//Route::post('/properties/search', [PropertyController::class, 'searchPost']);
+//Route::post('/properties/search-street', [PropertyController::class, 'searchByStreet']);
 
 
 Route::any('/properties', [PropertyController::class, 'index']);
